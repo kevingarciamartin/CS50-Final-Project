dfebab99342479da6c06f03c12e66741866d61f1
Kevin Garcia Martin <kevingm@live.se>
2023-04-18 20:12:18 +0200

    database

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ import os
 
 from flask import Flask, flash, redirect, render_template, request, session
 from flask_session import Session
+import sqlite3
+from werkzeug.security import check_password_hash, generate_password_hash
 
 from helpers import login_required
 
@@ -12,6 +14,10 @@ app = Flask(__name__)
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
+
+# Create connection to database
+connection = sqlite3.connect("app.db", check_same_thread=False)
+cursor = connection.cursor()
 
 @app.after_request
 def after_request(response):
@@ -24,6 +30,7 @@ def after_request(response):
 
 @app.route("/")
 def index():
+    """Home page"""
     return render_template("index.html")
 
 
@@ -59,7 +66,49 @@ def register():
     
     # POST 
     if request.method == "POST":
-        pass    
+        
+        username = request.form.get("username")
+        password = request.form.get("password")
+        confirmation = request.form.get("confirmation")
+        hash = generate_password_hash(password)
+        
+        # Ensure username was submitted
+        if not username:
+            return render_template("error.html", message="must provide username")
+        
+        # Ensure password was submitted
+        elif not password:
+            return render_template("error.html", message="must provide password")
+        
+        # Ensure password confirmation was submitted
+        elif not confirmation:
+            return render_template("error.html", message="must provide password confirmation")
+        
+        # Ensure password and confirmation match
+        elif password != confirmation:
+            return render_template("error.html", message="passwords don't match")
+        
+        # Ensure username doesn't exist
+        print(cursor.execute("SELECT * FROM users WHERE username = ?", [username]))
+        print(cursor.fetchone())
+        # elif  == True:
+        #     return render_template("error.html", message="username is taken")
+        
+        # Remember user 
+        cursor.execute("INSERT INTO users (username, hash) VALUES(?, ?)", [username, hash])
+        connection.commit()
+        
+        # Query database for username 
+        result = cursor.execute("SELECT * FROM users WHRE username = ?", [username])
+        
+        print(result.fetchone()) 
+        
+        # Remember which user has logged in
+        session["user_id"] = result[0]["id"]
+        
+        flash("Registered!")
+        
+        return redirect("/")  
     
     # GET
     else:
